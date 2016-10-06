32d9a404f8fb2744817fa23d671355980ffdc678
matsuhisa <matsuhisa+github@gmail.com>
2016-10-06 23:53:50 +0900

    Add public void sendMessage

--- app/src/main/java/com/example/matsuhisahironobu/inputapp01/MainActivity.java
+++ app/src/main/java/com/example/matsuhisahironobu/inputapp01/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.matsuhisahironobu.inputapp01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.view_message);
+        textView.setText(message);
     }
 }
